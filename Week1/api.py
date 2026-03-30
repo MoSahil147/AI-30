b831d6fab03eb622fbca8f65c68bf441fef5d46c
Sahil Mohapatra <f20220112@dubai.bits-pilani.ac.in>
2026-03-30 13:45:19 +0530

    Day 6: Refactored, rag_pipeline.py shared module, api.py cleaned up

--- Week1/api.py
+++ Week1/api.py
@@ -1,249 +1,319 @@
-### ITSSS day5 we are making APII
+# ### ITSSS day5 we are making APII
 
-## os se upar kuch nahiii
-import os 
-import sys
-import shutil
-from dotenv import load_dotenv
+
+# now we will be calling from rag_pipelineess
+import os
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from typing import Optional
+from dotenv import load_dotenv
+
+# will import things from the sared pipeline
+from rag_pipeline import (
+    load_pipeline,
+    expanded_hybrid_search,
+    rerank,
+    llm as pipeline_llm
+)
 
 load_dotenv()
 
-app = FastAPI(
+app=FastAPI(
     title="RAG API",
-    description="Ask question about the research papers, lets test my knowledge",
+    description="Ask questions about research papers",
     version="1.0.0"
 )
 
-# define what the user SENDS us
 class AskRequest(BaseModel):
     question:str
-
-# define what we send back
+    
 class AskResponse(BaseModel):
     question:str
     answer:str
     
-# health check endpoint
+# will loadanya bjojyam the pipeline at the startup
+PDF_PATH = "Paper/Detection_of_Fake_Accounts_on_Social_Media_Using_Multimodal_Data_With_Deep_Learning.pdf"
+llm=load_pipeline(PDF_PATH)
+
 @app.get("/health")
 def health_check():
-    return {"status":"ok","message":"RAG API is running"}
+    return {"status":"ok", "message":"RAG API is Runninnggg!!!"}
 
-# ask endpoint - main functionnn
 @app.post("/ask", response_model=AskResponse)
 def ask_question(request: AskRequest):
-    # will validate the inputs and will not accept empty questions
     if not request.question.strip():
         raise HTTPException(
             status_code=400,
             detail="Question cannot be empty"
         )
     try:
-        # same running the prev day4 pipeline
-        # request se jo question aa raha hai usse we are using as the query
-        candidates = expanded_hybrid_search(request.question, k=10)
-        best_chunks=rerank(request.question, candidates, top_k=3)
+        candidates=expanded_hybrid_search(request.question, k=10)
+        best_chunks=rerank(request.question, candidates, top_k=5)
         
-        # build the context from chunks
         context="\n\n".join([chunk.page_content for chunk in best_chunks])
-        
-        # build prompt
-        prompt = f"""Answer the question using only the context below.
-If the answer is not in the context, say "I don't know".
+        prompt=f"""Answer the question using only the context below.
+If answer is not in the context, simply say, "I don't know!".
 
-Context:
+Context: 
 {context}
-
+  
 Question: {request.question}
 Answer:"""
-        # call the daddy LLM directly
+
         response = llm.invoke(prompt)
-    
+        
         return AskResponse(
-        question=request.question,
-        answer=response.content
+            question=request.question,
+            answer=response.content
         )
-    
     except Exception as e:
         raise HTTPException(
             status_code=500,
             detail=f"Pipeline error: {str(e)}"
         )
+# ## os se upar kuch nahiii
+# import os 
+# import sys
+# import shutil
+# from dotenv import load_dotenv
+# from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
+# from typing import Optional
+
+# load_dotenv()
+
+# app = FastAPI(
+#     title="RAG API",
+#     description="Ask question about the research papers, lets test my knowledge",
+#     version="1.0.0"
+# )
+
+# # define what the user SENDS us
+# class AskRequest(BaseModel):
+#     question:str
+
+# # define what we send back
+# class AskResponse(BaseModel):
+#     question:str
+#     answer:str
     
-# ## (test : curl -X POST http://127.0.0.1:8000/ask \
-#   -H "Content-Type: application/json" \
-#   -d '{"question": "What API was used to collect Twitter data?"}')   
+# # health check endpoint
+# @app.get("/health")
+# def health_check():
+#     return {"status":"ok","message":"RAG API is running"}
+
+# # ask endpoint - main functionnn
+# @app.post("/ask", response_model=AskResponse)
+# def ask_question(request: AskRequest):
+#     # will validate the inputs and will not accept empty questions
+#     if not request.question.strip():
+#         raise HTTPException(
+#             status_code=400,
+#             detail="Question cannot be empty"
+#         )
+#     try:
+#         # same running the prev day4 pipeline
+#         # request se jo question aa raha hai usse we are using as the query
+#         candidates = expanded_hybrid_search(request.question, k=10)
+#         best_chunks=rerank(request.question, candidates, top_k=3)
+        
+#         # build the context from chunks
+#         context="\n\n".join([chunk.page_content for chunk in best_chunks])
+        
+#         # build prompt
+#         prompt = f"""Answer the question using only the context below.
+# If the answer is not in the context, say "I don't know".
+
+# Context:
+# {context}
+
+# Question: {request.question}
+# Answer:"""
+#         # call the daddy LLM directly
+#         response = llm.invoke(prompt)
+    
+#         return AskResponse(
+#         question=request.question,
+#         answer=response.content
+#         )
+    
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=500,
+#             detail=f"Pipeline error: {str(e)}"
+#         )
+    
+# # ## (test : curl -X POST http://127.0.0.1:8000/ask \
+# #   -H "Content-Type: application/json" \
+# #   -d '{"question": "What API was used to collect Twitter data?"}')   
         
    
-# rag prep 
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import Chroma
-from langchain_groq import ChatGroq
-from langchain_classic.chains import RetrievalQA
-from rank_bm25 import BM25Okapi
-from sentence_transformers import CrossEncoder
-from langchain_core.retrievers import BaseRetriever
-from langchain_core.documents import Document
-from typing import List
-from pydantic import Field
-import numpy as np
-import shutil
+# # rag prep 
+# from langchain_community.document_loaders import PyPDFLoader
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_huggingface import HuggingFaceEmbeddings
+# from langchain_community.vectorstores import Chroma
+# from langchain_groq import ChatGroq
+# from langchain_classic.chains import RetrievalQA
+# from rank_bm25 import BM25Okapi
+# from sentence_transformers import CrossEncoder
+# from langchain_core.retrievers import BaseRetriever
+# from langchain_core.documents import Document
+# from typing import List
+# from pydantic import Field
+# import numpy as np
+# import shutil
 
-# global variable
-# load once when server starts and reuse every request
-# pehle load kare phir vishwash kare
-vectorstore=None
-bm25=None
-unique_chunks=None
-reranker=None
-qa_chain=None
+# # global variable
+# # load once when server starts and reuse every request
+# # pehle load kare phir vishwash kare
+# vectorstore=None
+# bm25=None
+# unique_chunks=None
+# reranker=None
+# qa_chain=None
 
-def load_pipeline():
-    """Load the full RAG pipeline - called once at the startup"""
-    global vectorstore, bm25, unique_chunks, reranker, qa_chain
+# def load_pipeline():
+#     """Load the full RAG pipeline - called once at the startup"""
+#     global vectorstore, bm25, unique_chunks, reranker, qa_chain
     
-    print("Loading RAG pipeline....")
+#     print("Loading RAG pipeline....")
     
-    loader = PyPDFLoader("Paper/Detection_of_Fake_Accounts_on_Social_Media_Using_Multimodal_Data_With_Deep_Learning.pdf")
-    docs = loader.load()
+#     loader = PyPDFLoader("Paper/Detection_of_Fake_Accounts_on_Social_Media_Using_Multimodal_Data_With_Deep_Learning.pdf")
+#     docs = loader.load()
     
-    splitter=RecursiveCharacterTextSplitter(
-        chunk_size=500,
-        chunk_overlap=50
-    )
-    chunks = splitter.split_documents(docs)
+#     splitter=RecursiveCharacterTextSplitter(
+#         chunk_size=500,
+#         chunk_overlap=50
+#     )
+#     chunks = splitter.split_documents(docs)
     
-    # now remove dups dups
-    seen = set()
-    unique_chunks=[]
-    for chunk in chunks:
-        if chunk.page_content not in seen:
-            seen.add(chunk.page_content)
-            unique_chunks.append(chunk)
+#     # now remove dups dups
+#     seen = set()
+#     unique_chunks=[]
+#     for chunk in chunks:
+#         if chunk.page_content not in seen:
+#             seen.add(chunk.page_content)
+#             unique_chunks.append(chunk)
             
-    # build vector store, bhain kahi tho store karna hai nah
-    if os.path.exists("chroma_db"):
-        shutil.rmtree("chroma_db")
+#     # build vector store, bhain kahi tho store karna hai nah
+#     if os.path.exists("chroma_db"):
+#         shutil.rmtree("chroma_db")
     
-    embeddings=HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
-    vectorstore = Chroma.from_documents(
-        documents=unique_chunks,
-        embedding=embeddings,
-        persist_directory="chroma_db"
-    )
+#     embeddings=HuggingFaceEmbeddings(
+#         model_name="sentence-transformers/all-MiniLM-L6-v2"
+#     )
+#     vectorstore = Chroma.from_documents(
+#         documents=unique_chunks,
+#         embedding=embeddings,
+#         persist_directory="chroma_db"
+#     )
     
-    # build the BM25 index
-    tokenized_chunks = [
-        chunk.page_content.lower().split()
-        for chunk in unique_chunks
-    ]
-    # we did the firsts tep of tokeinsize, small case
-    bm25=BM25Okapi(tokenized_chunks)
+#     # build the BM25 index
+#     tokenized_chunks = [
+#         chunk.page_content.lower().split()
+#         for chunk in unique_chunks
+#     ]
+#     # we did the firsts tep of tokeinsize, small case
+#     bm25=BM25Okapi(tokenized_chunks)
     
-    # load reranker
-    reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+#     # load reranker
+#     reranker = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
     
-    # load LLM 
-    llm = ChatGroq(
-        api_key=os.getenv("GROQ_API_KEY"),
-        model_name="llama-3.3-70b-versatile",
-        temperature=0
-    )
+#     # load LLM 
+#     llm = ChatGroq(
+#         api_key=os.getenv("GROQ_API_KEY"),
+#         model_name="llama-3.3-70b-versatile",
+#         temperature=0
+#     )
 
-    print("Pipeline loaded successfullyyy!!!")
-    return llm
+#     print("Pipeline loaded successfullyyy!!!")
+#     return llm
 
-llm=load_pipeline()
+# llm=load_pipeline()
 
-# Block 3 - Search functions
-# same hybrid search + rerankung them
+# # Block 3 - Search functions
+# # same hybrid search + rerankung them
 
-def hybrid_search_single(query, k=10):
-    vector_results=vectorstore.similarity_search(query, k=10)
+# def hybrid_search_single(query, k=10):
+#     vector_results=vectorstore.similarity_search(query, k=10)
     
-    tokenized_query=query.lower().split()
-    bm25_scores=bm25.get_scores(tokenized_query)
-    top_bm25_indices=np.argsort(bm25_scores)[::-1][:10]
-    bm25_results=[unique_chunks[i] for i in top_bm25_indices]
+#     tokenized_query=query.lower().split()
+#     bm25_scores=bm25.get_scores(tokenized_query)
+#     top_bm25_indices=np.argsort(bm25_scores)[::-1][:10]
+#     bm25_results=[unique_chunks[i] for i in top_bm25_indices]
     
-    chunk_scores={}
-    for rank, chunk in enumerate(vector_results):
-        content = chunk.page_content
-        if content not in chunk_scores:
-            chunk_scores[content]=0
-        chunk_scores[content]+=1/(rank+60)
+#     chunk_scores={}
+#     for rank, chunk in enumerate(vector_results):
+#         content = chunk.page_content
+#         if content not in chunk_scores:
+#             chunk_scores[content]=0
+#         chunk_scores[content]+=1/(rank+60)
     
-    for rank, chunk in enumerate(bm25_results):
-        content=chunk.page_content
-        if content not in chunk_scores:
-            chunk_scores[content] = 0
-        chunk_scores[content]+=1/(rank+60)
+#     for rank, chunk in enumerate(bm25_results):
+#         content=chunk.page_content
+#         if content not in chunk_scores:
+#             chunk_scores[content] = 0
+#         chunk_scores[content]+=1/(rank+60)
     
-    sorted_contents=sorted(
-        chunk_scores.keys(),
-        key=lambda x: chunk_scores[x],
-        reverse=True
-    )[:k]
+#     sorted_contents=sorted(
+#         chunk_scores.keys(),
+#         key=lambda x: chunk_scores[x],
+#         reverse=True
+#     )[:k]
     
-    result_chunks=[]
-    for content in sorted_contents:
-        for chunk in unique_chunks:
-            if chunk.page_content==content:
-                result_chunks.append(chunk)
-                break
+#     result_chunks=[]
+#     for content in sorted_contents:
+#         for chunk in unique_chunks:
+#             if chunk.page_content==content:
+#                 result_chunks.append(chunk)
+#                 break
             
-    return result_chunks
+#     return result_chunks
 
-def expand_query(question):
-    prompt=f"""Generate 3 different search queries for this question.
-Each query should approach the topic from a completely different angle.
-Use different keywords, technical terms and perspectives.
-Return ONLY the 3 queries, one per line, no numbering, no explanation.
+# def expand_query(question):
+#     prompt=f"""Generate 3 different search queries for this question.
+# Each query should approach the topic from a completely different angle.
+# Use different keywords, technical terms and perspectives.
+# Return ONLY the 3 queries, one per line, no numbering, no explanation.
 
-Question: {question}
-"""
-    response=llm.invoke(prompt)
-    queries=response.content.strip().split("\n")
-    queries=[q.strip() for q in queries if q.strip()]
-    # returning question along with the query we got
-    return [question] + queries[:3]
+# Question: {question}
+# """
+#     response=llm.invoke(prompt)
+#     queries=response.content.strip().split("\n")
+#     queries=[q.strip() for q in queries if q.strip()]
+#     # returning question along with the query we got
+#     return [question] + queries[:3]
 
-def expanded_hybrid_search(question, k=10):
-    queries=expand_query(question)
-    all_chunk_scores={}
+# def expanded_hybrid_search(question, k=10):
+#     queries=expand_query(question)
+#     all_chunk_scores={}
     
-    for query in queries:
-        results = hybrid_search_single(query, k=10)
-        for rank, chunk in enumerate(results):
-            content = chunk.page_content
-            if content not in all_chunk_scores:
-                all_chunk_scores[content]=0
-            all_chunk_scores[content]+=1/(rank+60)
+#     for query in queries:
+#         results = hybrid_search_single(query, k=10)
+#         for rank, chunk in enumerate(results):
+#             content = chunk.page_content
+#             if content not in all_chunk_scores:
+#                 all_chunk_scores[content]=0
+#             all_chunk_scores[content]+=1/(rank+60)
             
-    sorted_contents= sorted(
-        all_chunk_scores.keys(),
-        key=lambda x: all_chunk_scores[x],
-        reverse=True
-    )[:k]
+#     sorted_contents= sorted(
+#         all_chunk_scores.keys(),
+#         key=lambda x: all_chunk_scores[x],
+#         reverse=True
+#     )[:k]
     
-    result_chunks=[]
-    for content in sorted_contents:
-        for chunk in unique_chunks:
-            if chunk.page_content == content:
-                result_chunks.append(chunk)
-                break
+#     result_chunks=[]
+#     for content in sorted_contents:
+#         for chunk in unique_chunks:
+#             if chunk.page_content == content:
+#                 result_chunks.append(chunk)
+#                 break
             
-    return result_chunks
+#     return result_chunks
 
-def rerank(query, chunks, top_k=3):
-    pairs=[[query, chunk.page_content] for chunk in chunks]
-    scores = reranker.predict(pairs)
-    ranked_indices=np.argsort(scores)[::-1][:top_k]
-    return [chunks[i] for i in ranked_indices]
+# def rerank(query, chunks, top_k=3):
+#     pairs=[[query, chunk.page_content] for chunk in chunks]
+#     scores = reranker.predict(pairs)
+#     ranked_indices=np.argsort(scores)[::-1][:top_k]
+#     return [chunks[i] for i in ranked_indices]
 
--- Week1/rag_pipeline.py
+++ Week1/rag_pipeline.py
@@ -1,8 +1,7 @@
 # this will be a shared pipelne used by both api.py and everyday scirpts
 # DRY principle: define once and import everywhere
 
-from day2_rag import unique_chunks
-from api import vectorstore, rerank
+# nothing above os
 import os
 import shutil
 from dotenv import load_dotenv
@@ -29,4 +28,139 @@ reranker=None
 llm=None
 
 def load_pipeline(pdf_path: str):
-    global vectorstore, bm25, 
+    global vectorstore, bm25, unique_chunks, reranker, llm
+    
+    print(".... Loading RAG PIPElinee.....")
+    
+    # 1 loader and doc
+    loader=PyPDFLoader(pdf_path)
+    docs = loader.load()
+    
+    # 2 Split
+    splitter=RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+    chunks=splitter.split_documents(docs)
+    
+    # 3 sirf unique picks!!
+    seen = set()
+    unique_chunks=[]
+    for chunk in chunks:
+        if chunk.page_content not in seen:
+            seen.add(chunk.page_content)
+            unique_chunks.append(chunk)
+       
+    # 4 chromadb should be empty     
+    if os.path.exists("chroma_db"):
+        shutil.rmtree("chroma_db")
+    
+    # 5 add embeddings and vectorstore    
+    embeddings= HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+    vectorstore=Chroma.from_documents(
+        documents=unique_chunks,
+        embedding=embeddings,
+        persist_directory="chroma_db"
+    )
+    
+    # 6 toknize for the bm25 (keywords)
+    tokenized_chunks=[
+        chunk.page_content.lower().split()
+        for chunk in unique_chunks
+    ]
+    bm25=BM25Okapi(tokenized_chunks)
+    reranker=CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+    
+    # 7 llm sir
+    llm=ChatGroq(
+        api_key=os.getenv("GROQ_API_KEY"),
+        model_name="llama-3.3-70b-versatile",
+        temperature=0
+    )
+    
+    print("Pipeline loadeddd successfullyyy")
+    return llm
+
+# time for hybrid search nowww
+def hybrid_search_single(query, k=10):
+    # first find similarity
+    vector_results=vectorstore.similarity_search(query, k=10)
+    # tokenize for BM25
+    tokenized_query=query.lower().split()
+    bm25_scores=bm25.get_scores(tokenized_query)
+    top_bm25_indices=np.argsort(bm25_scores)[::-1][:10]
+    bm25_results=[unique_chunks[i] for i in top_bm25_indices]
+    
+    chunk_scores={}
+    for rank, chunk in enumerate(vector_results):
+        content=chunk.page_content
+        if content not in chunk_scores:
+            chunk_scores[content]=0
+        chunk_scores[content]+=1/(rank+60)
+    
+    for rank, chunk in enumerate(bm25_results):
+        content=chunk.page_content
+        if content not in chunk_scores:
+            chunk_scores[content]=0
+        chunk_scores[content]+=1/(rank+60)
+        
+    sorted_contents=sorted(
+        chunk_scores.keys(),
+        key=lambda x:chunk_scores[x],
+        reverse=True
+    )[:k]
+    
+    result_chunks=[]
+    for content in sorted_contents:
+        for chunk in unique_chunks:
+            if chunk.page_content==content:
+                result_chunks.append(chunk)
+                break
+    return result_chunks
+    
+# now generating different questions to cross check
+def expand_query(question):
+    prompt=f"""Generate 3 different search queries for this question.
+Each query should approach the topic from a completely different angle.
+Use different keywords, technical terms and perspectives.
+Return ONLY the 3 queries, one per line, no numbering, no explanation.
+
+Question: {question}"""
+    response=llm.invoke(prompt)
+    queries=response.content.strip().split("\n")
+    queries=[q.strip() for q in queries if q.strip()]
+    return [question] + queries[:3]
+    
+def expanded_hybrid_search(question, k=10):
+    queries=expand_query(question)
+    all_chunk_scores={}
+    
+    for query in queries:
+        results=hybrid_search_single(query, k=10)
+        for rank, chunk in enumerate(results):
+            content=chunk.page_content
+            if content not in all_chunk_scores:
+                all_chunk_scores[content]=0
+            all_chunk_scores[content]+=1/(rank+60)
+    
+    sorted_contents=sorted(
+        all_chunk_scores.keys(),
+        key=lambda x:all_chunk_scores[x],
+        reverse=True
+    )[:k]
+    
+    result_chunks=[]
+    for content in sorted_contents:
+        for chunk in unique_chunks:
+            if chunk.page_content==content:
+                result_chunks.append(chunk)
+                break
+    return result_chunks
+    
+def rerank(query, chunks,top_k=3):
+    pairs=[[query, chunk.page_content] for chunk in chunks]
+    scores = reranker.predict(pairs)
+    ranked_indices=np.argsort(scores)[::-1][:top_k]
+    return [chunks[i] for i in ranked_indices]
